31c0bea060154356bab6f5ca263c0373abbd3911
황석진 <dollking@users.noreply.github.com>
2020-02-17 22:32:25 +0900

    add maker file

--- graphs/models/bar_v1/decoder.py
+++ graphs/models/bar_v1/decoder.py
@@ -12,38 +12,38 @@ class Decoder(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.sigmoid = nn.Sigmoid()
 
-        self.pitch1 = nn.ConvTranspose2d(in_channels=1152, out_channels=512, kernel_size=(1, 6), stride=(1, 6),
+        self.pitch1 = nn.ConvTranspose2d(in_channels=2304, out_channels=1024, kernel_size=(1, 6), stride=(1, 6),
                                          bias=False)
-        self.pitch2 = nn.ConvTranspose2d(in_channels=512, out_channels=512, kernel_size=(6, 1), stride=(6, 1),
+        self.pitch2 = nn.ConvTranspose2d(in_channels=1024, out_channels=1024, kernel_size=(6, 1), stride=(6, 1),
                                          bias=False)
 
-        self.time1 = nn.ConvTranspose2d(in_channels=1152, out_channels=512, kernel_size=(6, 1), stride=(6, 1),
+        self.time1 = nn.ConvTranspose2d(in_channels=2304, out_channels=1024, kernel_size=(6, 1), stride=(6, 1),
                                         bias=False)
-        self.time2 = nn.ConvTranspose2d(in_channels=512, out_channels=512, kernel_size=(1, 6), stride=(1, 6),
+        self.time2 = nn.ConvTranspose2d(in_channels=1024, out_channels=1024, kernel_size=(1, 6), stride=(1, 6),
                                         bias=False)
 
-        self.fit1 = nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=1, stride=1, bias=False)
-        self.batch_norm1 = nn.BatchNorm2d(512)
+        self.fit1 = nn.Conv2d(in_channels=2048, out_channels=1024, kernel_size=1, stride=1, bias=False)
+        self.batch_norm1 = nn.BatchNorm2d(1024)
 
-        self.deconv1 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=3, stride=2,
+        self.deconv1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=3, stride=2,
                                           padding=1, output_padding=1, bias=True)
-        self.deconv2 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=2,
-                                          padding=1, output_padding=1, bias=True)
-
-        self.fit2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=1, stride=1, bias=False)
-        self.batch_norm2 = nn.BatchNorm2d(128)
-
-        self.deconv3 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2,
-                                          padding=1, output_padding=1, bias=True)
-        self.deconv4 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=2,
+        self.deconv2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=3, stride=2,
                                           padding=1, output_padding=1, bias=True)
 
-        self.fit3 = nn.Conv2d(in_channels=32, out_channels=1, kernel_size=1, stride=1, bias=False)
+        self.fit2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=1, stride=1, bias=False)
+        self.batch_norm2 = nn.BatchNorm2d(256)
+
+        self.deconv3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=2,
+                                          padding=1, output_padding=1, bias=True)
+        self.deconv4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2,
+                                          padding=1, output_padding=1, bias=True)
+
+        self.fit3 = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, bias=False)
 
         self.apply(weights_init)
 
     def forward(self, x):
-        x = x.view(-1, 1152, 1, 1)
+        x = x.view(-1, 2304, 1, 1)
         pitch_out = self.leaky(self.pitch1(x))
         pitch_out = self.leaky(self.pitch2(pitch_out))
 
