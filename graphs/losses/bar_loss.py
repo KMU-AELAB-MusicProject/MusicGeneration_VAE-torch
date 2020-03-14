4cfaa2f734e3bc679005e654bb48741100b8b35f
황석진 <dollking@users.noreply.github.com>
2020-03-14 15:04:22 +0900

    add maker file

--- graphs/losses/bar_loss.py
+++ graphs/losses/bar_loss.py
@@ -11,8 +11,11 @@ class Loss(nn.Module):
         recon_loss = self.loss(logits, labels)
         elbo = (torch.sum(1 + var - mean.pow(2) - var.exp()) + torch.sum(1 + var - pre_mean.pow(2) - pre_var.exp())) / 2
 
+        out = torch.gt(logits, 0.35).type('torch.cuda.FloatTensor')
+        additional_loss = torch.gt(labels - out, 0.0001).type('torch.cuda.FloatTensor')
+
         # reconstruction error + KLD + gan_loss
-        return recon_loss - (0.5 * elbo)# - torch.log(gan_loss).mean()
+        return (recon_loss + additional_loss * 0.8) - (0.5 * elbo) # - torch.log(gan_loss).mean()
 
 
 class PhraseLoss(nn.Module):
@@ -23,8 +26,11 @@ class PhraseLoss(nn.Module):
     def forward(self, logits, labels, mean, var):
         recon_loss = self.loss(logits, labels)
 
+        out = torch.gt(logits, 0.35).type('torch.cuda.FloatTensor')
+        additional_loss = torch.gt(labels - out, 0.0001).type('torch.cuda.FloatTensor')
+
         # reconstruction error + KLD + gan_loss
-        return recon_loss - (0.5 * torch.sum(1 + var - mean.pow(2) - var.exp()))# - torch.log(gan_loss).mean()
+        return (recon_loss + additional_loss * 0.8) - (0.5 * torch.sum(1 + var - mean.pow(2) - var.exp()))# - torch.log(gan_loss).mean()
 
 
 class DLoss(nn.Module):
