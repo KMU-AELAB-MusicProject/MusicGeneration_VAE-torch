8ab69e8cc3d61fdb0c0d631781a0fe305eb620e6
황석진 <dollking@users.noreply.github.com>
2020-05-20 18:41:33 +0900

    wae-gan + gan loss - cbam base

--- agent/barGen.py
+++ agent/barGen.py
@@ -298,9 +298,9 @@ class BarGen(object):
         self.summary_writer.add_scalar("epoch/Bar_Z_Discriminator_loss", avg_barZ_disc_loss.val, self.epoch)
         self.summary_writer.add_scalar("epoch/Phrase_Z_discriminator_loss", avg_phraseZ_disc_loss.val, self.epoch)
 
-        self.summary_writer.add_image("epoch/sample image", image_sample[0].reshape(1, 96, 60), self.epoch)
-        self.summary_writer.add_image("epoch/sample image", image_sample[1].reshape(1, 96, 60), self.epoch)
-        self.summary_writer.add_image("epoch/sample image", image_sample[2].reshape(1, 96, 60), self.epoch)
+        self.summary_writer.add_image("epoch/sample image 1", image_sample[0].reshape(1, 96, 60), self.epoch)
+        self.summary_writer.add_image("epoch/sample image 2", image_sample[1].reshape(1, 96, 60), self.epoch)
+        self.summary_writer.add_image("epoch/sample image 3", image_sample[2].reshape(1, 96, 60), self.epoch)
 
         self.scheduler_gen.step(avg_gen_loss.val)
         self.scheduler_discriminator.step(avg_disc_loss.val)
