b2b6dc7cef246c65e4e1e2e457ae11dd6461447a
황석진 <dollking@users.noreply.github.com>
2020-05-21 14:41:13 +0900

    wae-gan + gan loss - cbam base

--- graph/loss/bar_loss.py
+++ graph/loss/bar_loss.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class Loss(nn.Module):
     def __init__(self):
         super().__init__()
-        self.loss = nn.MSELoss()
+        self.loss = nn.BCELoss()
 
     def forward(self, logits, labels, gan_loss):
         recon_loss = self.loss(logits, labels)
