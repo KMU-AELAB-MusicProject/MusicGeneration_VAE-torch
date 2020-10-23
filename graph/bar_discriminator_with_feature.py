e0c6cb9144d6a1e905019a8eff5ddbf37326ff9c
iodo <jnwochoi@gmail.com>
2020-10-23 16:53:05 +0900

    add gan method

---
+++ graph/bar_discriminator_with_feature.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+from graph.weights_initializer import weights_init
+
+class BarFeatureDiscriminator(nn.Module):
+    def __init__(self):
+        super(BarDiscriminator, self).__init__()
+
+        self.linear1 = nn.Linear(1152, 512, bias=False)
+        self.linear2 = nn.Linear(96, 1, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+        self.apply(weights_init)
+
+    def forward(self, x):
+        x = x.view(-1, 1152)
+
+        x1 = self.linear1(x)
+        x2 = self.linear2(x1)
+
+        outputs = self.sigmoid(x2)
+
+        return outputs
---
+++ graph/model_with_gan.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+
+from .decoder import Decoder
+from .encoder import Encoder
+from .phrase_encoder import PhraseModel
+from graph.weights_initializer import weights_init
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.encoder = Encoder([64, 128, 256, 512, 1024])
+        self.decoder = Decoder([1024, 512, 256, 128, 64])
+        self.phrase_encoder = PhraseModel([64, 128, 256, 512, 1024])
+
+        self.apply(weights_init)
+
+    def forward(self, note, pre_note, phrase, position, is_train=True):
+        if is_train:
+            phrase_feature = self.phrase_encoder(phrase)
+
+            z = self.encoder(note)
+            pre_z = self.encoder(pre_note)
+
+            gen_note = self.decoder(z, pre_z, phrase_feature, position)
+            fake_note = torch.gt(gen_note, 0.35).type('torch.cuda.FloatTensor')
+            return gen_note, z, pre_z, phrase_feature, self.encoder(fake_note)
+        else:
+            phrase_feature = self.phrase_encoder(phrase)
+
+            pre_z = self.encoder(pre_note)
+
+            return self.decoder(note, pre_z, phrase_feature, position)
